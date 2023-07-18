c760903c92e8a1c3d1d3abe1443f3427d587bbca
HantaoGG <2543960380@qq.com>
2023-07-18 22:19:29 +0800

    代码部分提交

---
+++ code/part_code/php/src/new_article.php
@@ -0,0 +1,59 @@
+<?php
+
+    $host = 'db';
+
+    // Database use name
+    $user = 'MYSQL_USER';
+
+    //database user password
+    $pass = 'MYSQL_PASSWORD';
+
+    // $conn=mysqli_connect("localhost","root","kali","articles");
+    $conn=mysqli_connect($host,$user,$pass,"blog");
+
+    include "session.php";
+
+    include "connect.php";
+
+    $title=$_POST['title'];
+
+    $author=$_SESSION['username'];
+
+    $content=$_POST['content'];
+
+    if($title&&$author&&$content){
+
+        $sql="INSERT INTO articles(title,author,content) VALUES('$title','$author','$content')";
+
+        $conn->query($sql);
+
+        ?>
+
+        <script>
+
+            alert("文章发布成功!");
+
+            window.location.href="personal.php";
+
+    </script>
+
+    <?php
+
+    }else{
+
+        ?>
+
+        <script>
+
+            alert("文章发布失败!标题、作者、内容可能为空！");
+
+            window.location.href="new_article.html";
+
+    </script>
+
+    <?php
+
+    }
+
+    ?>
+
